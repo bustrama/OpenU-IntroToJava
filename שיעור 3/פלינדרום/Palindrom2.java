01a5d895ea514de49f90158bdc278a4a326835dd
Ben Brandes <rubik.junk@gmail.com>
2019-03-13 20:12:59 +0200

    Update Palindrom2.java

--- שיעור 3/פלינדרום/Palindrom2.java
+++ שיעור 3/פלינדרום/Palindrom2.java
@@ -1,41 +1,26 @@
 import java.util.Scanner;
 
-public class Palindrom
-{
-   public static void main(String[]args)
-   {
-       Scanner scan = new Scanner(System.in);
-       int number,number1,firstDigit,newNum;
-       newNum=0;
+public class Palindrom {
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner(System.in);
 
-       System.out.println("please enter 5 numbers");
-       number = scan.nextInt();
-       number1= number;
-       
-       firstDigit = number%10;
-       newNum=(newNum+firstDigit)*10;
-       number=number/10;
-       
-       firstDigit = number%10;
-       newNum=(newNum+ firstDigit)*10;
-       number=number/10;
-       
-       firstDigit = number%10;
-       newNum=(newNum+ firstDigit)*10;
-       number=number/10;
-       
-       firstDigit = number%10;
-       newNum=(newNum+ firstDigit)*10;
-       number=number/10;
-       
-       firstDigit = number%10;
-       newNum=newNum+ firstDigit;
-       number=number/10;
-       
-       if (newNum == number1) {
-           System.out.println("the number is a palindrom");}
-           else{ 
-           System.out.println("the number is not a palindrom");
+        System.out.print("please enter a number with 5 digits: ");
+        int number = scan.nextInt();
+
+        int x1 = number / 10000 % 10;
+        int x2 = number / 1000 % 10;
+        int x3 = number / 100 % 10;
+        int x4 = number / 10 % 10;
+        int x5 = number % 10;
+
+        System.out.println(x1 + ", " + x2 + ", " + x3 + ", " + x4 + ", " + x5);
+
+
+        if (x1 == x5 && x2 == x4) {
+            System.out.println("the number is a palindrom");}
+        else{
+            System.out.println("the number is not a palindrom");
         }
     }
 }
