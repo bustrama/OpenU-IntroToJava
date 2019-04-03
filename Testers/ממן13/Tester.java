95e528ec1321a38fdc4ceb9cc37734e48f16be99
Ben Brandes <rubik.junk@gmail.com>
2019-04-04 01:30:57 +0300

    Updated mmn13 tester

--- Testers/ממן13/Tester.java
+++ Testers/ממן13/Tester.java
@@ -1,14 +1,129 @@
 /**
  * @author Ben
- * @version v1.0
+ * @version v1.2
  * שימו לב שבבדיקה של המטריקס, הבדיקה מסתמכת על toString אז יש לדאוג שהוא תקין
  */
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Tester {
     final static String GOOD = "\t\t\t OK!";
     final static String BAD = "\t\t\t ** Oh snap! something is broken **";
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int option = 0;
+
+        while (true) {
+            System.out.println("*****************************************");
+            System.out.println("*****         Mmn 13 Tester         *****");
+            System.out.println("*****************************************");
+            System.out.print("\n 1. Test Collection \n 2. Test Matrix \n 3. Test Both \n 4. Print Collection " +
+                    "\n 5. Print Matrix \n Choose an option: ");
+
+            try {
+                option = input.nextInt();
+            } catch (InputMismatchException err) {
+                System.out.println("\n ***    Enter a number smart pants    ***");
+                option = 0;
+                input.next();
+            }
+
+            if (option != 0) {
+                switch (option) {
+                    case 1:
+                        testCollection();
+                        break;
+                    case 2:
+                        testMatrix();
+                        break;
+                    case 3:
+                        testCollection();
+                        testMatrix();
+                        break;
+                    case 4:
+                        printCollection();
+                        break;
+                    case 5:
+                        printMatrix();
+                        break;
+                    default:
+                        System.out.println("Enter a number from the options above");
+                        break;
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void testCollection() {
+        System.out.println();
+        System.out.println("*******************************************");
+        System.out.println("****         Collection Tester         ****");
+        System.out.println("*******************************************");
+
+        Collection coll = new Collection();
+
+        for (int i = 0; i < 50; i++) {
+            boolean cont = coll.addBox(new Point3D(i,i+1,i+2), i+1, i, i+3);
+        }
+
+        System.out.println("mostUpperBaseCorner: ");
+        if(coll.mostUpperBaseCorner().toString().equals("The base point is (49.0,50.0,51.0), length = 50, width = 49, height = 52"))
+            System.out.println(GOOD);
+        else
+            System.out.println(BAD);
+
+        System.out.println("totalSurfaceArea: ");
+        if(coll.totalSurfaceArea() == 262458.0)
+            System.out.println(GOOD);
+        else
+            System.out.println(BAD);
+
+        System.out.println("longestDistance: " );
+        if(coll.longestDistance() == 106.44951855222267)
+            System.out.println(GOOD);
+        else
+            System.out.println(BAD);
+
+        System.out.println("howManyContains: ");
+        if(coll.howManyContains(new Box3D(new Point3D(2,2,2), 5,7,8)) == 42)
+            System.out.println(GOOD);
+        else
+            System.out.println(BAD);
+
+        System.out.println("volumeOfSmallestBox: " + coll.volumeOfSmallestBox(40, 50));
+
+        System.out.println("getNumOfBoxes: ");
+        if(coll.getNumOfBoxes() == 50)
+            System.out.println(GOOD);
+        else
+            System.out.println(BAD);
+    }
+
+    public static void printCollection(){
+        Collection coll = new Collection();
+
+        boolean condition = true;
+        Random rand = new Random();
+        while(condition) {
+            boolean cont = coll.addBox(new Point3D(rand.nextInt(50), rand.nextInt(50), rand.nextInt(50)), rand.nextInt(50), rand.nextInt(50), rand.nextInt(50));
+            if(!cont)
+                condition = false;
+        }
+
+        System.out.println(coll.toString());
+        System.out.println("Done printing 100 boxes with random values");
+
+        System.out.println("Methods: ");
+        System.out.println("mostUpperBaseCorner" + coll.mostUpperBaseCorner());
+        System.out.println("totalSurfaceArea" + coll.totalSurfaceArea());
+        System.out.println("howManyContains" + coll.howManyContains(new Box3D(new Point3D(2,2,2), 5,7,8)));
+        System.out.println("volumeOfSmallestBox" + coll.volumeOfSmallestBox(44, 68));
+        System.out.println("getNumOfBoxes" + coll.getNumOfBoxes());
+    }
 
     static int m[][] = {
             {19, 124, 28, 35, 38},
@@ -35,56 +150,6 @@ public class Tester {
             {100, 150, 200}
     };
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int option = 0;
-
-        while (true) {
-            System.out.println("*****************************************");
-            System.out.println("*****         Mmn 13 Tester         *****");
-            System.out.println("*****************************************");
-            System.out.print("\n 1. Test Collection \n 2. Test Matrix \n 3. Test Both \n 4. Print Matrix \n Choose an option: ");
-
-            try {
-                option = input.nextInt();
-            } catch (InputMismatchException err) {
-                System.out.println("\n ***    Enter a number smart pants    ***");
-                option = 0;
-                input.next();
-            }
-
-            if (option != 0) {
-                switch (option) {
-                    case 1:
-                        testCollection();
-                        break;
-                    case 2:
-                        testMatrix();
-                        break;
-                    case 3:
-                        testCollection();
-                        testMatrix();
-                        break;
-                    case 4:
-                        printMatrix();
-                        break;
-                    default:
-                        System.out.println("Enter a number from the options above");
-                        break;
-                }
-            }
-
-            System.out.println();
-        }
-    }
-
-    public static void testCollection() {
-        System.out.println();
-        System.out.println("*******************************************");
-        System.out.println("****         Collection Tester         ****");
-        System.out.println("*******************************************");
-    }
-
     public static void testMatrix() {
         System.out.println();
         System.out.println("*******************************************");
@@ -97,21 +162,21 @@ public class Tester {
         Matrix matr = new Matrix(ma);
 
         System.out.println("Regular Matrix:");
-        toCheck = "19\t124\t28\t35\t38\n115\t22\t25\t230\t31\n9\t21\t22\t249\t230\n0\t6\t9\t232\t255\n2\t5\t10\t116\t129";
+        toCheck = "19\t124\t28\t35\t38\n115\t22\t25\t230\t31\n9\t21\t22\t249\t230\n0\t6\t9\t232\t255\n2\t5\t10\t116\t129\n";
         if (toCheck.equals(mat.toString()))
             System.out.println(GOOD);
         else
             System.out.println(BAD);
 
         System.out.println("Negative Matrix:");
-        toCheck = "236\t131\t227\t220\t217\n140\t233\t230\t25\t224\n246\t234\t233\t6\t25\n255\t249\t246\t23\t0\n253\t250\t245\t139\t126";
+        toCheck = "236\t131\t227\t220\t217\n140\t233\t230\t25\t224\n246\t234\t233\t6\t25\n255\t249\t246\t23\t0\n253\t250\t245\t139\t126\n";
         if (toCheck.equals(mat.makeNegative().toString()))
             System.out.println(GOOD);
         else
             System.out.println(BAD);
 
         System.out.println("Filtered Image Matrix:");
-        toCheck = "70\t55\t77\t64\t83\n51\t42\t84\t98\t135\n28\t25\t90\t142\t204\n7\t9\t74\t139\t201\n3\t5\t63\t125\t183";
+        toCheck = "70\t55\t77\t64\t83\n51\t42\t84\t98\t135\n28\t25\t90\t142\t204\n7\t9\t74\t139\t201\n3\t5\t63\t125\t183\n";
         String toCheck2 = "71\t57\t62\t31";
         if (toCheck.equals(mat.imageFilterAverage().toString()) && toCheck2.equals(matr.imageFilterAverage().toString()))
             System.out.println(GOOD);
@@ -122,18 +187,18 @@ public class Tester {
         Matrix rot = new Matrix(rotate);
 
         System.out.println("90° Clockwise Matrix:");
-        toCheck = "62\t0\t19\t115\t19\n35\t16\t21\t22\t124\n10\t9\t22\t25\t28\n116\t232\t249\t230\t35";
-        toCheck2 = "100\t10\n150\t30\n200\t50";
-        String toCheck3 = "19\n124\n28\n35";
+        toCheck = "62\t0\t19\t115\t19\n35\t16\t21\t22\t124\n10\t9\t22\t25\t28\n116\t232\t249\t230\t35\n";
+        toCheck2 = "100\t10\n150\t30\n200\t50\n";
+        String toCheck3 = "19\n124\n28\n35\n";
         if (toCheck.equals(rot.rotateClockwise().toString()) && toCheck2.equals(m1.rotateClockwise().toString()) && toCheck3.equals(matr.rotateClockwise().toString()))
             System.out.println(GOOD);
         else
             System.out.println(BAD);
 
         System.out.println("90° Counter Clockwise Matrix:");
-        toCheck = "35\t230\t249\t232\t116\n28\t25\t22\t9\t10\n124\t22\t21\t16\t35\n19\t115\t19\t0\t62";
-        toCheck2 = "50\t200\n30\t150\n10\t100";
-        toCheck3 = "35\n28\n124\n19";
+        toCheck = "35\t230\t249\t232\t116\n28\t25\t22\t9\t10\n124\t22\t21\t16\t35\n19\t115\t19\t0\t62\n";
+        toCheck2 = "50\t200\n30\t150\n10\t100\n";
+        toCheck3 = "35\n28\n124\n19\n";
         if (toCheck.equals(rot.rotateCounterClockwise().toString()) && toCheck2.equals(m1.rotateCounterClockwise().toString()) && toCheck3.equals(matr.rotateCounterClockwise().toString()))
             System.out.println(GOOD);
         else
