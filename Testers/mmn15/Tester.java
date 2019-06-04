a094e1720eb92d1695372edd595d17d948b862f8
Ben Brandes <rubik.junk@gmail.com>
2019-06-04 22:53:43 +0300

    updated tester

--- Testers/mmn15/Tester.java
+++ Testers/mmn15/Tester.java
@@ -14,23 +14,34 @@ public class Tester {
         System.out.println("*****************************************");
 
         /*         AddNumber         */
-        System.out.println("Checking AddNumber functionality: ");
+        System.out.println("Checking addNumber functionality: ");
         if (checkAddNumber())
+            System.out.println(GOOD);
+        else
+            System.out.println(BAD);
+
+        /*         RemoveNumber         */
+        System.out.println("Checking removeNumber functionality: ");
+        if (checkRemoveNumber())
             System.out.println(GOOD);
         else
             System.out.println(BAD);
 
     }
 
-    private static boolean checkAddNumber() {
+    private static IntListTwo generateList(int size) {
         IntListTwo list = new IntListTwo();
         Random rnd = new Random();
 
         // Insert random numbers to the list
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < size; i++) {
             list.addNumber(rnd.nextInt(MAX + 1 - MIN) + MIN);
         }
 
+        return list;
+    }
+
+    private static int[] listToArray(IntListTwo list) {
         // Save the list to string and remove '{' and '}'
         String nodes = list.toString().replace("{", "").replace("}", "");
         // Replace all the ', ' with empty char
@@ -41,14 +52,80 @@ public class Tester {
             listArr[i] = Integer.parseInt(arr[i]);
         }
 
+        return listArr;
+    }
 
-        for (int i = 0; i < listArr.length - 1; i++) {
-            if (listArr[i] > listArr[i + 1]) {
+    private static boolean checkAddNumber() {
+        IntListTwo list = generateList(50);
+
+        int[] arrFromList = listToArray(list);
+
+        for (int i = 0; i < arrFromList.length - 1; i++) {
+            if (arrFromList[i] > arrFromList[i + 1]) {
                 System.out.println("The list: \n" + list.toString());
-                System.out.println(listArr[i] + " should be smaller than " + listArr[i + 1]);
+                System.out.println(arrFromList[i] + " should be smaller than " + arrFromList[i + 1]);
                 return false;
             }
         }
+
+        return true;
+    }
+
+    private static int[] removeIndex(int[] arr, int num) {
+        int[] newArr = new int[arr.length - 1];
+
+        boolean found = false;
+        int i = 0;
+        while (i < newArr.length) {
+            if (arr[i] == num)
+                found = true;
+
+            if (found)
+                newArr[i] = arr[i + 1];
+            else
+                newArr[i] = arr[i];
+
+            i++;
+        }
+
+        return newArr;
+    }
+
+    private static boolean compareListArr(IntListTwo list, int[] arr) {
+        int[] arrFromList = listToArray(list);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != arrFromList[i])
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean checkRemoveNumber() {
+        /*        Part 1        */
+        /* Remove head and tail */
+        IntListTwo list = generateList(20);
+        int[] arrFromList = listToArray(list);
+
+        list.removeNumber(arrFromList[0]);
+        arrFromList = removeIndex(arrFromList, arrFromList[0]);
+        if (!compareListArr(list, arrFromList))
+            return false;
+
+        list.removeNumber(arrFromList[arrFromList.length - 1]);
+        arrFromList = removeIndex(arrFromList, arrFromList[arrFromList.length - 1]);
+        if (!compareListArr(list, arrFromList))
+            return false;
+
+        /*       Part 2       */
+        /* Remove with 1 node */
+        IntListTwo list2 = generateList(1);
+        int[] arrFromList2 = listToArray(list2);
+
+        list2.removeNumber(arrFromList2[0]);
+        if (!list2.toString().equals("{}"))
+            return false;
+
 
         return true;
     }
