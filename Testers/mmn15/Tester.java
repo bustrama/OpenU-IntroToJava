937dbdb45c7a20c5c40b49f208ff611ab6eb1d1c
Matan <matanmunk@gmail.com>
2019-06-05 01:18:19 +0300

    Added IsAverage & added helper functions to reduce code
    as above + added smilies

--- Testers/mmn15/Tester.java
+++ Testers/mmn15/Tester.java
@@ -7,9 +7,17 @@ import java.util.Random;
 
 public class Tester {
     final static String GOOD = "\t\t\t\t\t\t\t\t\t OK!";
-    final static String BAD = "\t *** Oops, something's smelly with your node ***";
+    final static String BAD = "\t *** Oops, something's smelly with your node *** \n\t（╯°□°）╯ ︵ ┻━━┻";
     final static int MAX = 500;
     final static int MIN = -500;
+
+    static boolean passedTester = true;
+
+    //helper test function to reduce code
+    private static void test(boolean funcResult) {
+        System.out.println(funcResult ? GOOD : BAD);
+        passedTester = passedTester && funcResult;
+    }
 
     public static void main(String[] args) {
         System.out.println("*****************************************");
@@ -20,38 +28,34 @@ public class Tester {
 
         /*         AddNumber         */
         System.out.println("Checking addNumber functionality: ");
-        if (checkAddNumber())
-            System.out.println(GOOD);
-        else
-            System.out.println(BAD);
+        test(checkAddNumber());
 
         /*         RemoveNumber         */
         System.out.println("Checking removeNumber functionality: ");
-        if (checkRemoveNumber())
-            System.out.println(GOOD);
-        else
-            System.out.println(BAD);
+        test(checkRemoveNumber());
 
         /*         length         */
         System.out.println("Checking length functionality: ");
-        if (checkLength())
-            System.out.println(GOOD);
-        else
-            System.out.println(BAD);
+        test(checkLength());
 
         /*         sum         */
         System.out.println("Checking sum functionality: ");
-        if (checkSum())
-            System.out.println(GOOD);
-        else
-            System.out.println(BAD);
+        test(checkSum());
 
         /*         maxLength         */
         System.out.println("Checking maxLength functionality: ");
-        if (checkMaxLength())
-            System.out.println(GOOD);
-        else
-            System.out.println(BAD);
+        test(checkMaxLength());
+
+        /*         isAverage         */
+        System.out.println("Checking isAverage functionality: ");
+        test(checkIsAverage());
+
+        /*         passed Tester         */
+        if (passedTester){
+            System.out.println("(Still missing readToList test)");
+            System.out.println("\n Congrats! you've passed all the tests! \t ᕕ( ՞ ᗜ ՞ )ᕗ");
+        }
+
     }
 
     private static IntListTwo generateList(int size) {
@@ -183,13 +187,13 @@ public class Tester {
         IntListTwo list = generateList(20);
         int[] arr = listToArray(list);
 
-        if(list.sum() != sumArr(arr))
+        if (list.sum() != sumArr(arr))
             return false;
 
         IntListTwo list1 = generateList(1);
         int[] arr1 = listToArray(list1);
 
-        if(list1.sum() != sumArr(arr1))
+        if (list1.sum() != sumArr(arr1))
             return false;
 
         return true;
@@ -217,13 +221,89 @@ public class Tester {
         return temp;
     }
 
-    private static boolean checkMaxLength(){
+    private static boolean checkMaxLength() {
         IntListTwo list = generateList(1000);
         int[] arr = listToArray(list);
 
-        if(list.maxLength() != what(arr))
+        if (list.maxLength() != what(arr))
             return false;
 
         return true;
+    }
+
+
+    //counts on properly working IntListTwo!
+    private static boolean checkIsAverage() {
+
+        double tempAvg;
+
+        //test for empty list
+        IntListTwo list = generateList(0);
+
+        if (!list.isAverage(0)) {
+            printErr("Empty list isn't working");
+            return false;
+        }
+
+        list.addNumber(30);
+        list.addNumber(11);
+        list.addNumber(11);
+
+        //test for casting problems
+        if (list.isAverage((30 + 11 + 11) / 3)) {
+            printErr("You might have casting issues");
+            return false;
+        }
+
+        //casting works correctly
+        if (!list.isAverage((30.0 + 11 + 11) / 3)) {
+            printErr("make sure you average correctly");
+            return false;
+        }
+
+        list.addNumber(700);
+        list.addNumber(900);
+
+        //list should look like {11, 11, 30, 700, 900} (if people got this far it is probably correct)
+        //total sum so far is 1652, and 5 items
+
+        tempAvg = (30.0 + 11 + 11) / 3;
+
+        //check left subarray[0..2]
+        if (!list.isAverage(tempAvg)) {
+            printSubArrayErr(list, tempAvg, "left", "{11,11,30}");
+            return false;
+        }
+
+        tempAvg = (11.0 + 30 + 700) / 3;
+
+        //check middle subarray[1..3]
+        if (!list.isAverage(tempAvg)) {
+            printSubArrayErr(list, tempAvg, "middle", "{11, 30, 700}");
+            return false;
+        }
+
+        tempAvg = (30.0+700+900)/3;
+
+        //check right subarray[2..4]
+        if (!list.isAverage(tempAvg)) {
+            printSubArrayErr(list, tempAvg, "right", "{30, 700, 900}");
+            return false;
+        }
+
+        //TODO: maybe there are more edge cases to add
+        return true;
+    }
+
+    //save some code
+    private static void printSubArrayErr(IntListTwo list, double avg, String side, String subArray) {
+        printErr("your list is: " + list);
+        printErr("tested avg for " + avg);
+        printErr("you should have a " + side + " subarray with " + subArray + " with the correct avg, but got 'false'");
+    }
+
+    //too long to type
+    private static void printErr(String err) {
+        System.out.println("\t *** " + err);
     }
 }
